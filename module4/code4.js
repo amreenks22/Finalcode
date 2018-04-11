ad47ca0bbb9cb008d0acf640c47e986b65325596
amreenks22 <36256079+amreenks22@users.noreply.github.com>
2018-04-11 15:14:35 +0530

    Update code4.js

--- module4/code4.js
+++ module4/code4.js
@@ -1,5 +1,29 @@
 function onSubmit() {
 	//Type appropriate comment here, and begin script below
+	if ((newValue == "New Account Request") || (newValue == "Update to an existing account")) {
+		var Person = Backbone.Model.extend({
+		
+		    defaults: {  // Noncompliant; every instance of Person will share the same instance of favoriteColors
+        favoriteColors: ["blue","purple","raspberry"]
+    }
+});
+		function foo() {
+			var myArray = ['a', 'b', 'c', 'd'];
+delete myArray[2];  // Noncompliant. myArray => ['a', 'b', undefined, 'd']
+console.log(myArray[2]); 
+  let x = bar(1);
+		
+  if (x > 0) {
+      let x = bar(2); // Noncompliant
+      console.log(x);
+	  var a=3
+	  if(a=4){
+		  a=a+1;
+	  }
+  } else {
+     console.log("Wrong Value");
+  }
+}
 	
 	var blr = g_form.getValue('api_change');
 var dtc = g_form.getValue('api_change_rest_action_write'); 
